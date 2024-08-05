8b576b0c94fa75275d30c62625367062a765e212
zooway <70228762+zoooooway@users.noreply.github.com>
2024-08-05 17:23:55 +0800

    test

--- src/main/java/org/zoooooway/App.java
+++ src/main/java/org/zoooooway/App.java
@@ -1,53 +1,17 @@
 package org.zoooooway;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FFmpegFrameRecorder;
-import org.bytedeco.javacv.FFmpegLogCallback;
-import org.bytedeco.javacv.Frame;
-
-import static org.bytedeco.ffmpeg.global.avutil.AV_LOG_INFO;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Transcoding test via JavaCV
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String inputFile = "D:\\development\\temp\\video\\file_example_AVI_1920_2_3MG.avi";
         String outputFile = "D:\\development\\temp\\video\\result\\file_example_AVI_1920_2_3MG.mp4";
-        avi2mp4(inputFile, outputFile);
-    }
-
-    static void avi2mp4(String inputFile, String outputFile) {
-        FFmpegLogCallback.setLevel(AV_LOG_INFO);
-        FFmpegLogCallback.set();
-
-        try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile)) {
-            grabber.start();
-
-            try (FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(outputFile, grabber.getImageWidth(), grabber.getImageHeight(), grabber.getAudioChannels())) {
-                recorder.setVideoOption("threads", "4");
-                recorder.setFormat("mp4");
-                recorder.start();
-                Frame capturedFrame;
-                while (true) {
-                    capturedFrame = grabber.grabFrame();
-                    if (capturedFrame == null) {
-                        System.out.println("finish!");
-                        recorder.stop();
-                        recorder.release();
-                        grabber.stop();
-                        grabber.release();
-                        break;
-                    }
-                    recorder.record(capturedFrame);
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        VideoUtil.transcode(Files.newInputStream(Paths.get(inputFile)), Files.newOutputStream(Paths.get(outputFile)), "mp4", "mp4");
     }
 
 }
--- src/test/java/org/zoooooway/AppTest.java
+++ src/test/java/org/zoooooway/AppTest.java
@@ -4,35 +4,63 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws Exception {
+//        String inputURL = "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
+//        String inputURL = "https://merak.alicdn.com/video/understand-aliyun/for-incalculable-value.mp4";
+        String inputURL = "https://download.samplelib.com/mp4/sample-5s.mp4";
+        String outputFile = "D:\\development\\temp\\video\\result\\transcode.webm";
+
+        URL url = new URI(inputURL).toURL();
+
+        try (OutputStream os = Files.newOutputStream(Paths.get(outputFile))) {
+//            HttpClient httpClient = HttpClient.newBuilder().build();
+//            HttpRequest request
+//                    = HttpRequest.newBuilder()
+//                    .GET()
+//                    .uri(url.toURI())
+//                    .build();
+//            HttpResponse<InputStream> res = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+//            try (InputStream is = res.body()) {
+            try (InputStream is = Files.newInputStream(Paths.get("D:\\development\\temp\\video\\transcode.mp4"))) {
+                VideoUtil.realtimeTrans(is, os, "mp4", "webm");
+                Thread.sleep(60000);
+            }
+        }
+    }
+
+    public void testToMP4() throws Exception {
+        VideoUtil.transcode(new File("D:\\development\\temp\\video\\sample-5s.mp4"), new File("D:\\development\\temp\\video\\transcode.webm"), "mp4", "webm");
     }
 }
